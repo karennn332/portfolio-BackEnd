4a6841abb66d73b5d6d6a003223e9067ba19827e
karen <karennn332@gmail.com>
2023-05-05 14:06:18 -0300

    corrigiendo Cors en el back

--- src/main/java/com/miportfolio/portfolio/Controller/PersonaController.java
+++ src/main/java/com/miportfolio/portfolio/Controller/PersonaController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://frontendkd-1523f.web.app")
 public class PersonaController {
     @Autowired IPersonaServis ipersonaServis;
     
