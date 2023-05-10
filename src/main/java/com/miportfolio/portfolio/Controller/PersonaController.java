da770f0816a64af6a897e488e7d26e6577ad65f9
karen <karennn332@gmail.com>
2023-05-10 14:29:00 -0300

    complemento acerca de

--- src/main/java/com/miportfolio/portfolio/Controller/PersonaController.java
+++ src/main/java/com/miportfolio/portfolio/Controller/PersonaController.java
@@ -1,68 +1,92 @@
-
 package com.miportfolio.portfolio.Controller;
 
+import com.miportfolio.portfolio.Dto.dtoPersona;
 import com.miportfolio.portfolio.Entity.Persona;
-import com.miportfolio.portfolio.Interface.IPersonaServis;
+import com.miportfolio.portfolio.Security.Controller.Mensaje;
+import com.miportfolio.portfolio.Service.ImpPersonaService;
 import java.util.List;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/personas")
 @CrossOrigin(origins = "https://frontendkd-1523f.web.app")
-
-
 public class PersonaController {
-    @Autowired IPersonaServis ipersonaServis;
-    
-    @GetMapping("/personas/traer")
-    public List<Persona> getPersona(){
-        return ipersonaServis.getPersona();
-    }
-    @PreAuthorize("hasRole('ADMIN')")
-    @PostMapping("/crear")
-    public String createPersona(@RequestBody Persona persona){
-        
-        ipersonaServis.savePersona(persona);
-        return "La persona fue creada correctamente";
-    }
-    
-    @PreAuthorize("hasRole('ADMIN')")
-    @DeleteMapping("/personas/borrar/{id}")
-    public String deletePersona(@PathVariable Long id){
-            ipersonaServis.deletePersona(id);
-            return "La persona fue eliminada correctamente";
-    }
-    @PreAuthorize("hasRole('ADMIN')")
-    @PutMapping("/personas/editar/{id}")
-     public Persona editPersona(@PathVariable Long id, 
-             @RequestParam("nombre") String nuevoNombre,
-             @RequestParam("apellido") String nuevoApellido,
-             @RequestParam("img") String nuevoImg){
-     Persona persona = ipersonaServis.findPersona(id);
-     
-     
-     persona.setNombre(nuevoNombre);
-     persona.setApellido(nuevoApellido);
-     persona.setImg(nuevoImg);
-         
-             
-       ipersonaServis.savePersona(persona);
-       return persona;
-     
-     }
-     @GetMapping("/personas/traer/perfil")
-     public Persona findPersona() {
-         return ipersonaServis.findPersona((long)1);
-       
-     }
-}
 
+    @Autowired
+    ImpPersonaService personaService;
+
+    @GetMapping("/lista")
+    public ResponseEntity<List<Persona>> list() {
+        List<Persona> list = personaService.list();
+        return new ResponseEntity(list, HttpStatus.OK);
+    }
+
+    @GetMapping("/detail/{id}")
+    public ResponseEntity<Persona> getById(@PathVariable("id") int id) {
+        if (!personaService.existsById(id)) {
+            return new ResponseEntity(new Mensaje("no existe"), HttpStatus.NOT_FOUND);
+        }
+
+        Persona persona = personaService.getOne(id).get();
+
+        return new ResponseEntity(persona, HttpStatus.OK);
+    }
+
+    /* @PostMapping("/create")
+    public ResponseEntity<?> create(@RequestBody dtoExperiencia dtoexp){
+        if(StringUtils.isBlank(dtoexp.getNombreE()))
+            return new ResponseEntity(new Mensaje("El nombre es obligatorio"), HttpStatus.BAD_REQUEST);
+        if(sExperiencia.existsByNombreE(dtoexp.getNombreE()))
+            return new ResponseEntity(new Mensaje("Esa experiencia existe"), HttpStatus.BAD_REQUEST);
+        
+        Experiencia experiencia = new Experiencia(dtoexp.getNombreE(), dtoexp.getDescripcionE());
+        sExperiencia.save(experiencia);
+        
+        return new ResponseEntity(new Mensaje("Experiencia agregada "), HttpStatus.OK);
+    }*/
+    @PutMapping("/update/{id}")
+    public ResponseEntity<?> update(@PathVariable("id") int id, @RequestBody dtoPersona dtopersona) {
+        //validamos si esxiste el id
+        if (!personaService.existsById(id)) {
+            return new ResponseEntity(new Mensaje("El Id no existe"), HttpStatus.BAD_REQUEST);
+        }
+
+        //Compara nombre de experiencia
+        if (personaService.existsByNombre(dtopersona.getNombre()) && personaService.getByNombre(dtopersona.getNombre()).get().getId() != id) {
+            return new ResponseEntity(new Mensaje("Esa nombre experiencia ya existe"), HttpStatus.BAD_REQUEST);
+        }
+
+        //no puede estar vacio el campo 
+        if (StringUtils.isBlank(dtopersona.getNombre())) {
+            return new ResponseEntity(new Mensaje("el nombre es obligatorio"), HttpStatus.BAD_REQUEST);
+        }
+
+        Persona persona = personaService.getOne(id).get();
+
+        persona.setNombre(dtopersona.getNombre());
+        persona.setApellido(dtopersona.getApellido());
+        persona.setDescripcion((dtopersona.getDescripcion()));
+        persona.setImg(dtopersona.getImg());
+        personaService.save(persona);
+
+        return new ResponseEntity(new Mensaje("Persona actualizada"), HttpStatus.OK);
+    }
+    /*@DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> delete(@PathVariable("id") int id) {
+        if (!sExperiencia.existsById(id)) {
+            return new ResponseEntity(new Mensaje("no existe"), HttpStatus.NOT_FOUND);
+        }
+        sExperiencia.delete(id);
+        return new ResponseEntity(new Mensaje("producto eliminado"), HttpStatus.OK);
+    }*/
+}
--- src/main/java/com/miportfolio/portfolio/PortfolioApplication.java
+++ src/main/java/com/miportfolio/portfolio/PortfolioApplication.java
@@ -9,5 +9,6 @@ public class PortfolioApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PortfolioApplication.class, args);
 	}
+       
 
 }
