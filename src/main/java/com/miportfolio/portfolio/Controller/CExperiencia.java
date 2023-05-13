4824520c8e6195b95c285a2a5e94ec865e4729b2
karen <karennn332@gmail.com>
2023-05-13 00:01:21 -0300

    arreglos

--- src/main/java/com/miportfolio/portfolio/Controller/CExperiencia.java
+++ src/main/java/com/miportfolio/portfolio/Controller/CExperiencia.java
@@ -21,10 +21,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/explab")
 @CrossOrigin(origins = "https://frontendkd-1523f.web.app")
-
-
+@RequestMapping("/explab")
 public class CExperiencia {
     @Autowired
     SExperiencia sExperiencia;
